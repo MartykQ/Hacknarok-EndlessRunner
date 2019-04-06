ade600136d07e48050d1b9581f9a2ab710b5d9b9
Shinigami072 <krzys.stasiowski@gmail.com>
2019-04-06 17:56:14 +0200

    Pierwsza lakcja z tutoriala XD

---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController controller;
+
+    private float velocity = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        controller.Move(velocity*Vector3.forward * Time.deltaTime);
+    }
+}
