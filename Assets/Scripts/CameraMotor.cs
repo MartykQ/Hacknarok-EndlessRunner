41e5042328fad4ac12e8f526ebcf21a935df85a5
Shinigami072 <krzys.stasiowski@gmail.com>
2019-04-06 18:18:55 +0200

    Player controller

---
+++ Assets/Scripts/CameraMotor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class CameraMotor : MonoBehaviour
+{
+    private Transform lookAt;
+    private Vector3 offset;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       lookAt= GameObject.FindGameObjectWithTag("Player").transform;
+       offset = transform.position - lookAt.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = lookAt.position + offset;
+    }
+}
--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 public class PlayerMotor : MonoBehaviour
 {
     private CharacterController controller;
 
     private float velocity = 5;
+    private float verticalVelocity = 0;
+    private float gravity=2.0f;
+
+
+    private Vector3 moveVector;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +23,21 @@ public class PlayerMotor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        controller.Move(velocity*Vector3.forward * Time.deltaTime);
+
+        if (controller.isGrounded)
+        {
+            verticalVelocity = 0;
+        }
+        else
+        {
+            verticalVelocity -=gravity;
+        }
+
+        moveVector = Vector3.zero;
+        moveVector.z = velocity;
+        moveVector.y = verticalVelocity;
+        moveVector.x=Input.GetAxisRaw("Horizontal");
+        
+        controller.Move(moveVector * Time.deltaTime);
     }
 }
