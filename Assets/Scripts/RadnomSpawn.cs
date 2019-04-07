2f7f740cb2985fd1b41194cbd28866d4b75255d0
MartykQ <fmartyka16@gmail.com>
2019-04-07 02:09:58 +0200

    tiles rep

---
+++ Assets/Scripts/RadnomSpawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadnomSpawn : MonoBehaviour
+{
+
+
+    public Transform spawnerCords;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("asd");
+        Debug.Log(spawnerCords.position.x);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
